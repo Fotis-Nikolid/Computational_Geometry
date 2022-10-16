c6ba70ef01eb5dc86c1caebcc07e01db0d3df763
Fotis Nikolintais <57775853+Fotis-Nikolid@users.noreply.github.com>
2022-10-16 20:22:04 +0300

    Add files via upload

---
+++ incremental.cpp
@@ -0,0 +1,38 @@
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Polygon_2.h>
+#include <iostream>
+#include <vector>
+
+
+
+template<class Kernel>
+class Incremental {
+
+    typedef CGAL::Polygon_2<Kernel> Polygon_2;
+    typedef Kernel::Point_2  Point_2;
+    typedef Kernel::Segment_2 Segment;
+
+    private: 
+        //members
+        Polygon_2 Convex_Hull;
+        float Area;
+
+        //internal helper functions 
+        void Initialize(Polygon_2&) {
+
+        }
+        void Sort_Axis(std::vector<Point_2>,std::string) {
+
+        }
+        void Red_Edges(Polygon_2,Point_2) {
+
+        }
+        bool is_visible(Point_2,Point_2,Point_2) {
+
+        }
+    public:
+        float solve(Polygon_2& Polygon,std::vector<Point_2>& Points,std::string Criteria) {
+
+        }
+
+};
