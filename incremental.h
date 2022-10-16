9a6dc3a2a5d0d61ccea964d9802d14c65bc7ae99
Fotis Nikolintais <57775853+Fotis-Nikolid@users.noreply.github.com>
2022-10-16 20:20:07 +0300

    Add files via upload

---
+++ incremental.h
@@ -0,0 +1,25 @@
+#include <CGAL/Polygon_2.h>
+#include <iostream>
+#include <vector>
+
+template<class Kernel>
+class Incremental {
+    
+    typedef CGAL::Polygon_2<Kernel> Polygon_2;
+    typedef Kernel::Point_2  Point_2;
+    typedef Kernel::Segment_2 Segment;
+
+    private: 
+        //members
+        Polygon_2 Convex_Hull;
+        float Area;
+
+        //internal helper functions 
+        void Initialize(Polygon_2&);
+        void Sort_Axis(std::vector<Point_2>,std::string);
+        void Red_Edges(Polygon_2,Point_2);
+        bool is_visible(Point_2,Point_2,Point_2);
+        
+    public:
+        float solve(Polygon_2&,std::vector<Point_2>&,std::string);
+};
