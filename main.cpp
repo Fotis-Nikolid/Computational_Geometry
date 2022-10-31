f0e0051cfa0886937b7aa2ade623866a074385a3
JohnKalesi1 <sdi1900069@di.uoa.gr>
2022-10-31 21:13:52 +0200

    Finished Main

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <vector>
 #include <list>
+#include <chrono>
+#include <regex>
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Polygon_2.h>
 #include "polygon.h"
@@ -72,6 +74,11 @@ int main(int argc, char *argv[])
   std::string line;
   getline(infile,line);
   getline(infile,line);
+  double convex_hull_area;
+  std::smatch match_obj;
+  if (std::regex_search(line,match_obj,std::regex("\"([0-9]*)\""))) {
+      convex_hull_area=atof(match_obj[1].str().c_str());
+  }
   while (getline(infile,line)) {
       double x,y;
       int row;
@@ -81,26 +88,42 @@ int main(int argc, char *argv[])
       sline >> y;
 
       v_points.push_back(Point_2(x,y));
-      l_points.push_back(Point_2(x,y));
   }
+  
+  std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
   Polygon<K> poly;
+  Polygon_2 real_poly;
   if (algorithm=="hull") {
     poly.Algorithm(v_points,edge_selection);    
   }
   else {
     poly.Algorithm(v_points,edge_selection,sorting);
   }
+  std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 
-    std::cout<<poly.Size()<<std::endl;
-    if (poly.Simple()) {
-      if(poly.Size() == v_points.size())
-      {
-        std::cout<<"Correct"<<std::endl;
-      }
-    }
-    else {
-      std::cout<<"Error"<<std::endl;
-    }
+  real_poly=poly.get_Polygon();
+  if (poly.Simple() && poly.Size() == v_points.size()) {
+    std::cout<<"Correct"<<std::endl;
+  }
+  else {
+    std::cout<<"Error"<<std::endl;
+  }
+
+  outfile<<"Polygonization"<<std::endl;
+  for (const Point_2 p: real_poly.vertices()) {
+    outfile<<p<<std::endl;
+  }
+  for (const Segment_2 edge: real_poly.edges()) {
+    outfile<<edge<<std::endl;
+  }
+  outfile<<"Algorithm: "<<algorithm<<"_edge_selection_"<<edge_selection;
+  if (algorithm=="incremental") {
+    outfile<<"_initialization_"<<sorting;
+  }
+  outfile<<std::endl;
+  outfile<<"area: "<<poly.Area()<<std::endl;
+  outfile<<"ratio: "<<poly.Area()/convex_hull_area<<std::endl;
+  outfile<<"construction time: "<<std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count()<<std::endl;
 
   return 0;
 }
