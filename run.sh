2f42952e0f4072b0caff7a1a5293aa86b5e8b4de
Fotis-Nikolid <fotisnikolidais@gmail.com>
2022-11-01 15:36:06 +0200

    Run For incremental

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 cgal_create_CMakeLists -s to_polygon
 cmake -DCGAL_DIR=/usr/lib/CGAL
 make
-./to_polygon -i $1 -o $2 -algorithm $3 -edge_selection $4 
+./to_polygon -i $1 -o $2 -algorithm $3 -edge_selection $4 -initialization $5
