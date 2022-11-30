ffa7117eaafe9ad46cac66174e70935e0369c18d
JohnKalesi1 <sdi1900069@di.uoa.gr>
2022-11-30 17:11:42 +0200

    Added Comments

--- simulated_annealing.cpp
+++ simulated_annealing.cpp
@@ -15,12 +15,12 @@
 
 
 template<class Kernel> 
-bool comp_func(const CGAL::Point_2<Kernel> p1, const CGAL::Point_2<Kernel> p2) {
+bool comp_func(const CGAL::Point_2<Kernel> p1, const CGAL::Point_2<Kernel> p2) {//used for topological sorting by x-axis in increasing order
     return (p1.x()>p2.x());
 }
 
 template<class Kernel>
-double calculate_energy(double Area,double Hull_Area,std::string Criteria,int p_Size) {
+double calculate_energy(double Area,double Hull_Area,std::string Criteria,int p_Size) {//"energy" of a polygon state following formula used in Simulated Annealing
     if (Criteria=="max") {
         return p_Size*(1 - Area/Hull_Area);
     }
@@ -45,12 +45,12 @@ bool Simulated_Annealing::validity_check(CGAL::Triangle_2<Kernel> t1,CGAL::Trian
     return true;
 }
 
-//breaks the set of points into multiple subsets for which smaller polygon's will be created using global steps and then joined to then perform local step
+//Breaks the set of points into multiple subsets for which smaller polygon's will be created using global steps and then joined to then perform local steps
 template<class Kernel>
 std::vector<std::vector<CGAL::Point_2<Kernel>>> Simulated_Annealing::point_subsets(std::vector<CGAL::Point_2<Kernel>> Points) {
     std::sort(Points.begin(), Points.end(), comp_func<Kernel>);
-    int m=100;
-    int k=std::ceil((Points.size()-1)/(m-1));
+    int m=100;//average number of points per subset
+    int k=std::ceil((Points.size()-1)/(m-1));//desirable number of subsets created, depending on the orientation of the points this is not always possible
     std::vector<std::vector<Point_2>> subsets;
     int subset_n=1;
     for (int i=0;i<Points.size()-2;i++) {
@@ -58,67 +58,74 @@ std::vector<std::vector<CGAL::Point_2<Kernel>>> Simulated_Annealing::point_subse
         p1=Points.at(i);
         p2=Points.at(i+1);
         p3=Points.at(i+2);
-        if (p1.y()<p2.y() && p3.y()<p2.y()) {
-            if (i>(subset_n*m*0.9)) {
-                subsets[subset_n].append(p1);
-                subsets[subset_n].append(p2);//add the connecting point of the two subsets
+        if (p1.y()<p2.y() && p3.y()<p2.y()) {//if next 3 consecutive points form the pattern needed for breaking into subsets
+            if (i>(subset_n*m*0.9)) {//only break if around the point expected of a subset break or afterwards
+                subsets[subset_n].push_back(p1);
+                subsets[subset_n].push_back(p2);//add the connecting point of the two subsets
 
                 //subsets[subset_n+1].append(p2); //these two operations will occure naturally in the next two loops
                 //subsets[subset_n+1].append(p3);
-                subset_n++;
+                subset_n++;//following iterations will add points  
             }
         }
-        else {
-            subsets[subset_n].append(p1);
+        else {//if criteria for breaking is not met, simple add the point to current subset
+            subsets[subset_n].push_back(p1);
         }
         
     }
+    return subsets;
     
 }
 
+//find 2 consecutive points and swap their positions
 template<class Kernel>
 double Simulated_Annealing::local_step(CGAL::Polygon_2<Kernel>& Polygon) {
     while (true) {
-        int rand=rand()/Polygon.vertices().size();
+        int rand=rand()/Polygon.vertices().size();//pick a random point of the polygon, to begin the swap in the 2 following points
         typename Polygon_2::Vertices::iterator prev,swap1,swap2,next;
         Point_2 swap_t;
         
         prev=Polygon.vertices().start();
-        prev=std::next(prev,rand);
+        prev=std::next(prev,rand);//point which is previous to the two consecutive points being swapped
         
         auto iter=prev;
-        for (int i=0;i<3;i++) {
+        for (int i=0;i<3;i++) {//get the iterators of the 3 following points
             iter++;
-            if (iter==Polygon.vertices().end()) {
+            if (iter==Polygon.vertices().end()) {//given that some of the points might be after the end of the polygon(meaning we circle back), restart the iterator
                 iter=polygon.vertices().start();
             }
             switch (i) {
                 case 0:
-                    swap1=iter;
+                    swap1=iter;//1st follwing point to be swapped 
                     break;
                 case 1:
-                    swap2=iter;
+                    swap2=iter;//2nd following point to be swapped with 1st
                     break;
                 case 2:
-                    next=iter;
+                    next=iter;//point after the 2 that will be swapped
                     break;
             }
         }
-
+        //perform the swap
         swap_t=*swap2;
         *swap2=*swap1;
         *swap1=swap_t;
 
         std::vector<Point_2> points;
-        //get points inside the rectangle
+        
+        //in order to check if result creates a simple point, we must check whether any point exists inside of the two "triangles" being formed by the swap operation
+        //however, as to minimize the amount of points this validity check should be performed
+        //we use kd-trees to get only the points that exist inside the rectangle created by the four points
+        
         //points=...
 
         Triangle_2 t1,t2;
         t1=Triangle_2(*prev,*swap1,*swap2);
         t2=Triangle_2(*swap1,*swap2,*next);
-        if (!validity_check(t1,t2,points)) {
-            return Polygon.area();
+        if (!validity_check(t1,t2,points)) {//check if there new polygon is simple
+            return Polygon.area();//return new polygon area and thus exit the loop
         }
+        //if resulting polygon is not simple, the while loop will restart
     }
 }   
 
@@ -130,7 +137,9 @@ double Simulated_Annealing::global_step(CGAL::Polygon_2<Kernel>& Polygon) {
 template<class Kernel>
 void Simulated_Annealing::sub_division(CGAL::Polygon_2<Kernel>& Polygon,std::vector<CGALL::Point_2<Kernel>> Points,std::string Criteria,int Iterations) {
     std::vector<std::vector<Point_2>> Subsets;
-    Subsets=point_subsets(Points);
+    
+    Subsets=point_subsets(Points);//break points into multiple subsets
+    
     std::list<Polygon_2> polygons;
     
     char crit;
@@ -144,27 +153,36 @@ void Simulated_Annealing::sub_division(CGAL::Polygon_2<Kernel>& Polygon,std::vec
         crit='1';
     }
     
+    int subset_n=0;
     for (auto subset:Subsets) {
         Polygon_2 poly;
         
-        Incremental inc(subset,'1a',crit,subset[subset.size()-2]);
-        if (inc.getPolygonArea()==0) {//incremental failed
-            Hull hull;
-            std::list<Point_2> points(subset.begin(),subset.end());
-            Edge_2 e1,e2;
-            e1=Edge_2(subset[0],subset[1]);
-            e2=Edge_2(subset[subset.size()-1],subset[subset.size()-2]);
-            hull.solve(poly,points,crit,e1,e2);
+        Hull hull;
+        std::list<Point_2> points(subset.begin(),subset.end());
+        Edge_2 e1,e2;
+        e1=Edge_2(subset[0],subset[1]);//first edge in subset
+        e2=Edge_2(subset[subset.size()-1],subset[subset.size()-2]);//last edge in subset
+        //use altered version of Hull algorithm which will make sure not to break any of the two lines needed for the criteria to work
+        if (subset_n==0) {
+            hull.solve(poly,points,crit,NULL,e2);//only keep the last edge(first does not matter in the 1st subset)
+        }   
+        else if (subset) {
+            hull.solve(poly,points,crit,e1,NULL)//only keep the first edge(last does not matter in the last subset)
         }
-
-        solve(poly,Criteria,"global",Iterations);
+        else {
+            hull.solve(poly,points,crit,e1,e2);//keep both edges
+        }
+        solve(poly,Criteria,"global",Iterations);//solve subset based on global 
 
         polygons.append(poly);
         
-    }
+    } 
+    //merge the polygons
+    //apply local step 
     
 }
 
+//perform one attemp with local or global with either min max or random starting polygon
 template<class Kernel>
 double Simulated_Annealing::solve(CGAL::Polygon_2<Kernel>& Polygon,std::vector<CGAL::Point_2<Kernel>> Points,std::string Criteria,std::string Step_Choice,int Iterations) {
     srand((unsigned)time(NULL));
@@ -184,13 +202,13 @@ double Simulated_Annealing::solve(CGAL::Polygon_2<Kernel>& Polygon,std::vector<C
         crit='1';
     }
     
-    Incremental<Kernel> inc(Points,"1a",crit);//create a polygon which maximimes or minimizes total area
+    Incremental<Kernel> inc(Points,"1a",crit);//create a polygon which maximimes/minimizes/randomizes total area
     Polygon=inc.getPolygon();
      
     int size=Polygon.vertices().size();
     double area=inc.getPolygonArea();
     
-    double energy=calculate_energy(area,hull_area,Criteria,size);
+    double energy=calculate_energy(area,hull_area,Criteria,size);//calculate initial energy state of starting polygon
     
     double Temperature=1;
     while (Temperature>=0) {
@@ -203,15 +221,15 @@ double Simulated_Annealing::solve(CGAL::Polygon_2<Kernel>& Polygon,std::vector<C
             else if (Step_Choice=="global") {
                 n_area=global_step(t_Polygon);
             }
-            n_energy=calculate_energy(n_area,hull_area,Criteria,size);
+            n_energy=calculate_energy(n_area,hull_area,Criteria,size);//find energy of altered polygon
             
-            EnergyDifference=n_energy-energy;
-            if (EnergyDifference<0) {
-                break;
+            EnergyDifference=n_energy-energy;//energy difference
+            if (EnergyDifference<0) {//if change is positive
+                break;//keep solution
             }
-            else {
+            else {//else if change is negative, only apply it by performing the Metropolis criteria
                 double R=(double rand())/RAND_MAX
-                double threshold=std::exp(-EnergyDifference/Temperature);
+                double threshold=std::exp(-EnergyDifference/Temperature);//Metropolis formula
                 if (R<=threshold) {
                     break;
                 }
@@ -226,10 +244,11 @@ double Simulated_Annealing::solve(CGAL::Polygon_2<Kernel>& Polygon,std::vector<C
     return area;
 }
 
+//perform mulitple attempts with randomized starting polygons and pick the best polygon out of all
 template<class Kernel>
 double Simulated_Annealing::solve(CGAL::Polygon_2<Kernel>& Polygon,std::vector<CGAL::Point_2<Kernel>> Points,std::string Criteria,std::string Step_Choice,int Iterations,int Attempts) {
     Polygon_2 best_poly;
-    double p_area=(Criteria=="max"?0:std::numeric_limits<double>::max());
+    double p_area=(Criteria=="max"?0:std::numeric_limits<double>::max());//initialize area depending on maximization or minimization
     for (int i=0;i<Attempts;i++) {
         double area=solve(Polygon,Points,'random',Step_Choice,Iterations);
         if (Criteria=="max" && area>p_area) {
