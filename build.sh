43df2b14e60c8f370290438c034fe968d53a54cb
JohnKalesi1 <sdi1900069@di.uoa.gr>
2022-12-04 18:37:48 +0200

    Adjusted for release version

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 cgal_create_CMakeLists -s optimal_polygon
-cmake -DCGAL_DIR=-DCGAL_DIR=/usr/lib/CGAL -DCMAKE_BUILD_TYPE=Debug
+cmake -DCGAL_DIR=-DCGAL_DIR=/usr/lib/CGAL -DCMAKE_BUILD_TYPE=Release
 make
